5c21a021be65c03dddc6953af40db25e8b363b7b
nicknigredo <98839975+nicknigredo@users.noreply.github.com>
2025-04-30 20:50:45 +0300

    Box.js

--- docs/Box.js
+++ docs/Box.js
@@ -1,64 +1,31 @@
 export default class Box {
-  constructor(map, latlng, params) {
+  constructor(map, latlng, data) {
     this.map = map;
     this.latlng = latlng;
-    this.params = params;
-
-    // Размер бокса в пикселях
-    this.width = 60;
-    this.height = 60;
-
-    this._create();
+    this.data = data;
+    this.createSVGElement();
   }
 
-  _create() {
-    const svgIcon = L.divIcon({
+  createSVGElement() {
+    const icon = L.divIcon({
       className: '',
-      html: this._getSVG(),
-      iconSize: [this.width, this.height],
-      iconAnchor: [this.width / 2, this.height / 2]
+      html: `
+        <svg width="50" height="50">
+          <rect class="box-rect" x="5" y="5" width="40" height="40"></rect>
+          <text x="25" y="30" text-anchor="middle" fill="white" font-size="12" font-weight="bold">${this.data.id}</text>
+        </svg>
+      `,
+      iconSize: [50, 50]
     });
 
-    this.marker = L.marker(this.latlng, {
-      icon: svgIcon,
-      draggable: false
-    }).addTo(this.map);
+    const marker = L.marker(this.latlng, { icon }).addTo(this.map);
+    marker.bindPopup(`
+      <b>Бокс №${this.data.id}</b><br>
+      Спліттер: ${this.data.splitter}<br>
+      Адреса: ${this.data.address}<br>
+      Місце: ${this.data.location}
+    `);
 
-    this.marker.on('dblclick', () => this._showDetails());
-  }
-
-  _getSVG() {
-    const { id } = this.params;
-    return `
-      <svg width="${this.width}" height="${this.height}" xmlns="http://www.w3.org/2000/svg">
-        <rect x="0" y="0" width="${this.width}" height="${this.height}" fill="#3399ff" stroke="black" stroke-width="2" />
-        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle"
-              font-size="12" font-weight="bold" fill="white">${id}</text>
-
-        <!-- Точки привязки -->
-        ${this._anchorPoints().map(pt => `
-          <circle cx="${pt[0]}" cy="${pt[1]}" r="3" fill="black" />
-        `).join('')}
-      </svg>
-    `;
-  }
-
-  _anchorPoints() {
-    // Центр, четыре угла и середины сторон (8 точек привязки)
-    const w = this.width;
-    const h = this.height;
-    return [
-      [w/2, h/2],          // центр
-      [0, 0], [w, 0],      // верхние углы
-      [0, h], [w, h],      // нижние углы
-      [w/2, 0], [w/2, h],  // середины по вертикали
-      [0, h/2], [w, h/2]   // середины по горизонтали
-    ];
-  }
-
-  _showDetails() {
-    const { id, splitter, address, location } = this.params;
-    const msg = `Бокс: ${id}\nСпліттер: ${splitter}\nАдреса: ${address}\nМісце: ${location}`;
-    alert(msg);
+    this.marker = marker;
   }
 }
